5846b3d43d1a334b7f3014470e2d40e58878be23
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-03-01 17:40:15 +0900

    feat: stock class, struct

---
+++ CStock.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "CStock.h"
+
+CStock::CStock()
+{
+}
+
+CStock::~CStock()
+{
+}
---
+++ CStock.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define MAX_DATA (250)
+#define MAX_COMPANY (2500)
+
+struct Data {
+	long date; // 날짜
+	long startVal;
+	long highVal;
+	long lowVal;
+	long lastVal;
+	long vol;// 거래량
+};
+
+// 특정 종목. 수량과 날짜별 데이터를 소유.
+struct Company {
+	CString strJongMok, strName;
+	int quantity;
+	Data data[MAX_DATA];
+};
+
+struct AllCompany {
+	int quantity;
+	Company companies[MAX_COMPANY];
+};
+
+class CStock
+{
+// 생성자와 소멸자
+public:
+	CStock();
+	virtual ~CStock();
+// 멤버 변수
+public:
+	AllCompany allCompanies;
+};
+
